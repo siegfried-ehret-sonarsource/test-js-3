1b2a71ed4ddadfcfe03e97ec302bb3668c2d2198
Siegfried Ehret <siegfried.ehret@sonarsource.com>
2019-05-21 10:16:18 +0200

    plop

---
+++ index2.js
@@ -0,0 +1,17 @@
+console.log('Hello SonarSource!');
+function doStuff(a) {
+    console.log(a);
+}
+console.log('Hello SonarSource!');
+doStuff("plop");
+console.log('Hello SonarSource!');
+console.log('Hello SonarSource!');
+if (true) {
+    console.log('true');
+}
+
+console.log('pouet');
+
+if (true) {
+    console.log('true');
+}
